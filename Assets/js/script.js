3838d0f124f048ca9b27f968d7fc4c5ed8515b4b
Kalynn Powell <128936392+kmarie0420@users.noreply.github.com>
2023-05-04 17:44:55 -0500

    added comments to js and html file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assigned criteria for variables. ex: a hat filled with these characters.
 var generateBtn = document.querySelector("#generate");
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
 var lower = "abcdefghijklmnopqrstuvwxyz".split('')
@@ -8,13 +8,15 @@ var symbols = "!@#$%^&*()_+".split('')
 function genPassword() {
   var pre = []
   var post = ""
+  // Added prompt for criteria wanted, so its selectable and optionable.
   var passwordLength = prompt("How long would you like your password to be? (8-128)")
   if (passwordLength < 8 || passwordLength > 128) {
+    // Basically error message to follow minimum standards. 
     alert("Your password must be between 8-128.")
     genPassword()
   } else {
     if (confirm("Do you want uppercase letters in your password?")) {
-      // .concat takes a value, and adds to the end of it ex: "ca".concat("t") = "cat"
+      // .concat takes a value, and adds to the end of it. ex: "ca".concat("t") = "cat"
       pre = pre.concat(upper)
     }
     if (confirm("Do you want lowercase letters in your password?")) {
@@ -26,10 +28,12 @@ function genPassword() {
     if (confirm("Do you want symbols in your password?")) {
       pre = pre.concat(symbols)
     }
+    // If they do not select a character at ALL, they will get this prompt to retrieve persoanlized password. 
     if (pre.length === 0) {
       alert("Please choose one type of character to generate a password.")
       genPassword()
     } else {
+      // Starting a minimum base. [0]
       for (let i = 0; i < passwordLength; i++) {
         var rando = Math.floor(Math.random() * pre.length)
         post += pre[rando]
@@ -39,7 +43,7 @@ function genPassword() {
   }
 }
 
-// Write password to the #password input
+// wrote genPassword in place of password
 function writePassword() {
   var password = genPassword();
   var passwordText = document.querySelector("#password");
@@ -47,5 +51,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Added event listener to generate password. Clickable button 
 generateBtn.addEventListener("click", writePassword)
